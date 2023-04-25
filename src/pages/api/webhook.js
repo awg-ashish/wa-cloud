e52c9387dea80a580199609a1d92b886fe2246de
Ashish <ashish.blackhawk@gmail.com>
2023-04-25 19:19:31 +0530

    added relevant comments and phone no input field

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -2,122 +2,136 @@ import express from "express";
 import axios from "axios";
 
 export const config = {
-    api: {
-        bodyParser: false,
-    },
+  api: {
+    bodyParser: false,
+  },
 };
 // Function to fetch media from WhatsApp Cloud API
 async function fetchMedia(mediaId, accessToken) {
-    try {
-        const response = await axios.get(
-            `https://graph.facebook.com/v16.0/${mediaId}/`,
-            {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            }
-        );
-        const mediaUrl = response.data?.url;
-        return mediaUrl;
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
+  try {
+    const response = await axios.get(
+      `https://graph.facebook.com/v16.0/${mediaId}/`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const mediaUrl = response.data?.url;
+    return mediaUrl;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 export default async function handler(req, res) {
-    // Parse the request body using express.json() middleware
-    const bodyPromise = new Promise((resolve, reject) => {
-        express.json()(req, res, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(req.body);
-            }
-        });
+  // Parse the request body using express.json() middleware
+  const bodyPromise = new Promise((resolve, reject) => {
+    express.json()(req, res, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(req.body);
+      }
     });
+  });
 
-    if (req.method === "POST") {
-        const body = await bodyPromise;
+  if (req.method === "POST") {
+    const body = await bodyPromise;
 
-        // Process the webhook payload here...
+    // Process the webhook payload here...
 
-        //receiving messages
-        console.log(body.entry[0].changes[0].value);
-        if (body.entry) {
-            console.log("-------------------------------------------------");
-            console.dir(body.entry[0].changes[0].value.messages[0]);
-            console.log("-------------------------------------------------");
-            console.log("Field->", body.entry[0].changes[0].field);
-            console.log(
-                "Message Id->",
-                body?.entry[0].changes[0].value.messages[0].id
-            );
-            console.log(
-                "Time Sent->",
-                Date(body?.entry[0].changes[0].value.messages[0].timestamp)
-            );
-            console.log(
-                "Type->",
-                body?.entry[0].changes[0].value.messages[0].type
-            );
-            console.log(
-                "Name->",
-                body?.entry[0].changes[0].value.contacts[0].profile.name
-            );
-            console.log(
-                "Phone Number->",
-                body?.entry[0].changes[0].value.contacts[0].wa_id
-            );
-            console.log(
-                "messageBody->",
-                body?.entry[0].changes[0].value.messages[0].text?.body
-            );
-            console.log(
-                "Media Id->",
-                body?.entry[0].changes[0].value.messages[0].image?.id
-            );
-        }
+    //response and status of sent messages
 
-        // Getting the media id for media messages
-        const mediaId = body?.entry[0].changes[0].value.messages[0].image?.id;
-        if (mediaId) {
-            const mediaUrl = await fetchMedia(
-                mediaId,
-                process.env.WHATSAPP_TOKEN
-            );
-            console.log("Media URL -> ", mediaUrl);
-            //// Now you can use the media URL to download the media file
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-                },
-                responseType: "arraybuffer", // specify the response type as arraybuffer
-            };
+    //receiving messages
 
-            try {
-                const response = await axios.get(mediaUrl, config);
-                const mediaData = response.data; // binary data of media
-                // // Now you can use the mediaData to display the media or save it in the database
-                console.log(mediaData);
-            } catch (error) {
-                // handle error
-            }
-        }
+    // 1. Text Messages
 
-        // Respond with a 200 OK status code to acknowledge receipt of the webhook
-        res.status(200).end();
-    } else if (req.method === "GET") {
-        if (
-            req.query["hub.mode"] === "subscribe" &&
-            req.query["hub.verify_token"] === process.env.VERIFY_TOKEN
-        ) {
-            res.status(200).send(req.query["hub.challenge"]);
-        } else {
-            res.status(403).send("Invalid verify token");
-        }
-    } else {
-        // Respond with a 405 Method Not Allowed status code for non-POST requests
-        res.setHeader("Allow", "POST");
-        res.status(405).end();
+    console.log(body.entry[0].changes[0].value);
+    if (
+      body.entry[0].changes[0].value.messages &&
+      body.entry[0].changes[0].value.messages[0].type === "text"
+    ) {
+      console.log(
+        "----------------------INCOMING MESSAGE - TEXT---------------------------"
+      );
+      console.log(
+        "Name->",
+        body?.entry[0].changes[0].value.contacts[0].profile.name
+      );
+      console.log(
+        "Phone Number->",
+        `+${body?.entry[0].changes[0].value.contacts[0].wa_id}`
+      );
+      console.dir(body.entry[0].changes[0].value.messages[0]);
+      console.log(
+        "---------------------------------------------------------------------------"
+      );
     }
+
+    // 2. Media Messages
+
+    if (
+      body.entry[0].changes[0].value.messages &&
+      body.entry[0].changes[0].value.messages[0].type === "image"
+    ) {
+      const mediaId = body?.entry[0].changes[0].value.messages[0].image?.id;
+      if (mediaId) {
+        const mediaUrl = await fetchMedia(mediaId, process.env.WHATSAPP_TOKEN);
+        // Now you can use the media URL to download the media file
+        const config = {
+          headers: {
+            Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+          },
+          responseType: "arraybuffer", // specify the response type as arraybuffer
+        };
+
+        try {
+          const response = await axios.get(mediaUrl, config);
+          const mediaData = response.data; // Binary data of media. Now the mediaData can be used to display the media or can be saved in the database
+
+          console.log(
+            "-----------------------------INCOMING MESSAGE - MEDIA------------------------------"
+          );
+          console.log(
+            "Name->",
+            body?.entry[0].changes[0].value.contacts[0].profile.name
+          );
+          console.log(
+            "Phone Number->",
+            `+${body?.entry[0].changes[0].value.contacts[0].wa_id}`
+          );
+          body.entry[0].changes[0].value.messages[0].image.caption
+            ? console.log(
+                "Caption->",
+                body.entry[0].changes[0].value.messages[0].image.caption
+              )
+            : "";
+          console.log("Image Data in Binary->", mediaData);
+          console.dir(body.entry[0].changes[0].value.messages[0]);
+          console.log(
+            "--------------------------------------------------------------------------------------"
+          );
+        } catch (error) {
+          // handle error
+        }
+      }
+    }
+
+    // Respond with a 200 OK status code to acknowledge receipt of the webhook
+    res.status(200).end();
+  } else if (req.method === "GET") {
+    // required verification from Meta
+    if (
+      req.query["hub.mode"] === "subscribe" &&
+      req.query["hub.verify_token"] === process.env.VERIFY_TOKEN
+    ) {
+      res.status(200).send(req.query["hub.challenge"]);
+    } else {
+      res.status(403).send("Invalid verify token");
+    }
+  } else {
+    // Respond with a 405 Method Not Allowed status code for non-POST/GET requests
+    res.status(405).end();
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,47 +8,53 @@ import axios from "axios";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const bearer = process.env.NEXT_PUBLIC_WHATSAPP_TOKEN;
-    const [messageText, setMessageText] = useState("");
-    const sendMessage = async () => {
-        console.log("Sending..");
-        console.log(bearer);
-        try {
-            const response = await axios.post(
-                "https://graph.facebook.com/v16.0/111369568531545/messages",
-                {
-                    messaging_product: "whatsapp",
-                    recipient_type: "individual",
-                    to: "919471070789",
-                    type: "text",
-                    text: {
-                        // the text object
-                        preview_url: false,
-                        body: `${messageText}`,
-                    },
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${bearer}`,
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            console.log(response.data);
-            setMessageText("");
-        } catch (error) {
-            console.error(error);
+  const bearer = process.env.NEXT_PUBLIC_WHATSAPP_TOKEN;
+  const [messageText, setMessageText] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const sendMessage = async () => {
+    try {
+      const response = await axios.post(
+        "https://graph.facebook.com/v16.0/111369568531545/messages",
+        {
+          messaging_product: "whatsapp",
+          recipient_type: "individual",
+          to: `91${phoneNumber}`,
+          type: "text",
+          text: {
+            // the text object
+            preview_url: false,
+            body: `${messageText}`,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${bearer}`,
+            "Content-Type": "application/json",
+          },
         }
-    };
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder="Type your message here..."
-                value={messageText}
-                onChange={(e) => setMessageText(e.target.value)}
-            />
-            <button onClick={sendMessage}>Send</button>
-        </div>
-    );
+      );
+      console.log(response.data);
+      setMessageText("");
+      setPhoneNumber("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Phone Number..."
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Type your message here..."
+        value={messageText}
+        onChange={(e) => setMessageText(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
 }
