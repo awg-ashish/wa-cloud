b3309c5907de5d3b6d380d6d7fa456f3cb5d8b18
Ashish Anand <ashish.blackhawk@gmail.com>
2023-04-28 15:32:02 +0530

    added and retrieved messages from firebase

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,179 +1,223 @@
 import express from "express";
 import axios from "axios";
 import app from "../../../firebase";
-import {
-  addDoc,
-  collection,
-  getFirestore,
-  doc,
-  setDoc,
-} from "firebase/firestore";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 const db = getFirestore(app);
 export const config = {
-  api: {
-    bodyParser: false,
-  },
+    api: {
+        bodyParser: false,
+    },
 };
+
 // Function to fetch media from WhatsApp Cloud API
 async function fetchMedia(mediaId, accessToken) {
-  try {
-    const response = await axios.get(
-      `https://graph.facebook.com/v16.0/${mediaId}/`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    const mediaUrl = response.data?.url;
-    return mediaUrl;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+    try {
+        const response = await axios.get(
+            `https://graph.facebook.com/v16.0/${mediaId}/`,
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        const mediaUrl = response.data?.url;
+        return mediaUrl;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
+
+// Function to parse the request body using express.json() middleware
 export default async function handler(req, res) {
-  // Parse the request body using express.json() middleware
-  const bodyPromise = new Promise((resolve, reject) => {
-    express.json()(req, res, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(req.body);
-      }
+    const bodyPromise = new Promise((resolve, reject) => {
+        express.json()(req, res, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(req.body);
+            }
+        });
     });
-  });
 
-  if (req.method === "POST") {
-    const body = await bodyPromise;
+    // Processing the webhook payload...
 
-    // Process the webhook payload here...
+    if (req.method === "POST") {
+        const body = await bodyPromise;
 
-    //response and status of sent messages
-    if (body.entry[0].changes[0].value.statuses) {
-      // const outgoingDataToFirestore = {
-      //   messageId: body.entry[0].changes[0].value.statuses[0].wa_id,
-      //   category: "OUTGOING",
-      //   type: body.entry[0].changes[0].value.messages[0].type,
-      //   status: "received",
-      //   statusUpdatedOn: body.entry[0].changes[0].value.messages[0].timestamp,
-      //   timestamp: body.entry[0].changes[0].value.messages[0].timestamp,
-      //   body: body.entry[0].changes[0].value.messages[0].text,
-      // };
+        //response and status of sent messages
+        if (body.entry[0].changes[0].value.statuses) {
+            console.log(
+                "--------------------------------RESPONSE-----------------------"
+            );
+            console.log(body.entry[0].changes[0].value);
+            if (
+                body.entry[0].changes[0].value.statuses[0].conversation &&
+                body.entry[0].changes[0].value.statuses[0].pricing
+            ) {
+                const responseFromOutgoingData = {
+                    status: body.entry[0].changes[0].value.statuses[0].status,
+                    statusUpdatedOn:
+                        body.entry[0].changes[0].value.statuses[0].timestamp,
+                    conversation:
+                        body.entry[0].changes[0].value.statuses[0].conversation
+                            ?.origin.type,
+                    billable:
+                        body.entry[0].changes[0].value.statuses[0].pricing
+                            ?.billable,
+                };
+                const messageRef = doc(
+                    db,
+                    "messages",
+                    "ashish.blackhawk@gmail.com",
+                    body.entry[0].changes[0].value.statuses[0].recipient_id,
+                    body.entry[0].changes[0].value.statuses[0].id
+                );
+                setDoc(messageRef, responseFromOutgoingData, {
+                    merge: true,
+                });
+            } else {
+                const responseFromOutgoingData = {
+                    status: body.entry[0].changes[0].value.statuses[0].status,
+                    statusUpdatedOn:
+                        body.entry[0].changes[0].value.statuses[0].timestamp,
+                };
+                const messageRef = doc(
+                    db,
+                    "messages",
+                    "ashish.blackhawk@gmail.com",
+                    body.entry[0].changes[0].value.statuses[0].recipient_id,
+                    body.entry[0].changes[0].value.statuses[0].id
+                );
+                setDoc(messageRef, responseFromOutgoingData, {
+                    merge: true,
+                });
+            }
 
-      //receiving messages
-
-      // 1. Text Messages
-
-      console.log(body.entry[0].changes[0].value);
-      console.log(body.entry[0].changes[0].value.statuses[0].conversation);
-      console.log(body.entry[0].changes[0].value.statuses[0].pricing);
-    }
-    if (
-      body.entry[0].changes[0].value.messages &&
-      body.entry[0].changes[0].value.messages[0].type === "text"
-    ) {
-      console.log(
-        "----------------------INCOMING MESSAGE - TEXT---------------------------"
-      );
-      console.log(
-        "Name->",
-        body?.entry[0].changes[0].value.contacts[0].profile.name
-      );
-      console.log(
-        "Phone Number->",
-        `+${body?.entry[0].changes[0].value.contacts[0].wa_id}`
-      );
-      console.dir(body.entry[0].changes[0].value.messages[0]);
-      console.log(
-        "---------------------------------------------------------------------------"
-      );
-      //preparing the data for adding in firebase
-      const incomingDataToFirestore = {
-        messageId: body.entry[0].changes[0].value.contacts[0].wa_id,
-        category: "INCOMING",
-        type: body.entry[0].changes[0].value.messages[0].type,
-        status: "received",
-        statusUpdatedOn: body.entry[0].changes[0].value.messages[0].timestamp,
-        timestamp: body.entry[0].changes[0].value.messages[0].timestamp,
-        body: body.entry[0].changes[0].value.messages[0].text,
-      };
-      //adding text data to firebase
-      const messageRef = doc(
-        db,
-        "messages",
-        "ashish.blackhawk@gmail.com",
-        body.entry[0].changes[0].value.contacts[0].wa_id,
-        body.entry[0].changes[0].value.messages[0].id
-      );
-      setDoc(messageRef, incomingDataToFirestore, {
-        merge: true,
-      });
-    }
-
-    // 2. Media Messages
-
-    if (
-      body.entry[0].changes[0].value.messages &&
-      body.entry[0].changes[0].value.messages[0].type === "image"
-    ) {
-      const mediaId = body?.entry[0].changes[0].value.messages[0].image?.id;
-      if (mediaId) {
-        const mediaUrl = await fetchMedia(mediaId, process.env.WHATSAPP_TOKEN);
-        // Now you can use the media URL to download the media file
-        const config = {
-          headers: {
-            Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-          },
-          responseType: "arraybuffer", // specify the response type as arraybuffer
-        };
-
-        try {
-          const response = await axios.get(mediaUrl, config);
-          const mediaData = response.data; // Binary data of media. Now the mediaData can be used to display the media or can be saved in the database
-
-          console.log(
-            "-----------------------------INCOMING MESSAGE - MEDIA------------------------------"
-          );
-          console.log(
-            "Name->",
-            body?.entry[0].changes[0].value.contacts[0].profile.name
-          );
-          console.log(
-            "Phone Number->",
-            `+${body?.entry[0].changes[0].value.contacts[0].wa_id}`
-          );
-          body.entry[0].changes[0].value.messages[0].image.caption
-            ? console.log(
-                "Caption->",
-                body.entry[0].changes[0].value.messages[0].image.caption
-              )
-            : "";
-          console.log("Image Data in Binary->", mediaData);
-          console.dir(body.entry[0].changes[0].value.messages[0]);
-          console.log(
-            "--------------------------------------------------------------------------------------"
-          );
-        } catch (error) {
-          // handle error
+            console.log(
+                body.entry[0].changes[0].value.statuses[0].conversation
+            );
+            console.log(body.entry[0].changes[0].value.statuses[0].pricing);
+            console.log(
+                "--------------------------------------------------------------"
+            );
         }
-      }
-    }
 
-    // Respond with a 200 OK status code to acknowledge receipt of the webhook
-    res.status(200).end();
-  } else if (req.method === "GET") {
-    // required verification from Meta
-    if (
-      req.query["hub.mode"] === "subscribe" &&
-      req.query["hub.verify_token"] === process.env.VERIFY_TOKEN
-    ) {
-      res.status(200).send(req.query["hub.challenge"]);
+        //receiving messages
+
+        // 1. Text Messages
+
+        if (
+            body.entry[0].changes[0].value.messages &&
+            body.entry[0].changes[0].value.messages[0].type === "text"
+        ) {
+            console.log(
+                "----------------------INCOMING MESSAGE - TEXT---------------------------"
+            );
+            console.log(
+                "Name->",
+                body?.entry[0].changes[0].value.contacts[0].profile.name
+            );
+            console.log(
+                "Phone Number->",
+                `+${body?.entry[0].changes[0].value.contacts[0].wa_id}`
+            );
+            console.dir(body.entry[0].changes[0].value.messages[0]);
+            console.log(
+                "---------------------------------------------------------------------------"
+            );
+            //preparing the data for adding in firebase
+            const incomingDataToFirestore = {
+                messageId: body.entry[0].changes[0].value.contacts[0].wa_id,
+                category: "INCOMING",
+                type: body.entry[0].changes[0].value.messages[0].type,
+                status: "received",
+                statusUpdatedOn:
+                    body.entry[0].changes[0].value.messages[0].timestamp,
+                timestamp: body.entry[0].changes[0].value.messages[0].timestamp,
+                body: body.entry[0].changes[0].value.messages[0].text.body,
+            };
+            //adding text data to firebase
+            const messageRef = doc(
+                db,
+                "messages",
+                "ashish.blackhawk@gmail.com",
+                body.entry[0].changes[0].value.contacts[0].wa_id,
+                body.entry[0].changes[0].value.messages[0].id
+            );
+            setDoc(messageRef, incomingDataToFirestore, {
+                merge: true,
+            });
+        }
+
+        // 2. Media Messages
+
+        if (
+            body.entry[0].changes[0].value.messages &&
+            body.entry[0].changes[0].value.messages[0].type === "image"
+        ) {
+            const mediaId =
+                body?.entry[0].changes[0].value.messages[0].image?.id;
+            if (mediaId) {
+                const mediaUrl = await fetchMedia(
+                    mediaId,
+                    process.env.WHATSAPP_TOKEN
+                );
+                // Now you can use the media URL to download the media file
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+                    },
+                    responseType: "arraybuffer", // specify the response type as arraybuffer
+                };
+
+                try {
+                    const response = await axios.get(mediaUrl, config);
+                    const mediaData = response.data; // Binary data of media. Now the mediaData can be used to display the media or can be saved in the database
+
+                    console.log(
+                        "-----------------------------INCOMING MESSAGE - MEDIA------------------------------"
+                    );
+                    console.log(
+                        "Name->",
+                        body?.entry[0].changes[0].value.contacts[0].profile.name
+                    );
+                    console.log(
+                        "Phone Number->",
+                        `+${body?.entry[0].changes[0].value.contacts[0].wa_id}`
+                    );
+                    body.entry[0].changes[0].value.messages[0].image.caption
+                        ? console.log(
+                              "Caption->",
+                              body.entry[0].changes[0].value.messages[0].image
+                                  .caption
+                          )
+                        : "";
+                    console.log("Image Data in Binary->", mediaData);
+                    console.dir(body.entry[0].changes[0].value.messages[0]);
+                    console.log(
+                        "--------------------------------------------------------------------------------------"
+                    );
+                } catch (error) {
+                    // handle error
+                }
+            }
+        }
+
+        // Respond with a 200 OK status code to acknowledge receipt of the webhook
+        res.status(200).end();
+    } else if (req.method === "GET") {
+        // required verification from Meta
+        if (
+            req.query["hub.mode"] === "subscribe" &&
+            req.query["hub.verify_token"] === process.env.VERIFY_TOKEN
+        ) {
+            res.status(200).send(req.query["hub.challenge"]);
+        } else {
+            res.status(403).send("Invalid verify token");
+        }
     } else {
-      res.status(403).send("Invalid verify token");
+        // Respond with a 405 Method Not Allowed status code for non-POST/GET requests
+        res.status(405).end();
     }
-  } else {
-    // Respond with a 405 Method Not Allowed status code for non-POST/GET requests
-    res.status(405).end();
-  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,142 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "../styles/Home.module.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import app from "../../firebase";
 import {
-  addDoc,
-  collection,
-  getFirestore,
-  doc,
-  setDoc,
+    collection,
+    getFirestore,
+    doc,
+    setDoc,
+    Timestamp,
+    query,
+    orderBy,
+    onSnapshot,
 } from "firebase/firestore";
 const db = getFirestore(app);
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const bearer = process.env.NEXT_PUBLIC_WHATSAPP_TOKEN;
-  const [messageText, setMessageText] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const sendMessage = async () => {
-    try {
-      console.log(bearer);
-      console.log(`trying...`);
-      console.log(phoneNumber);
-      console.log(messageText);
-      const response = await axios.post(
-        "https://graph.facebook.com/v16.0/111369568531545/messages",
-        {
-          messaging_product: "whatsapp",
-          recipient_type: "individual",
-          to: `91${phoneNumber}`,
-          type: "text",
-          text: {
-            // the text object
-            preview_url: false,
-            body: `${messageText}`,
-          },
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${bearer}`,
-            "Content-Type": "application/json",
-          },
+    const bearer = process.env.NEXT_PUBLIC_WHATSAPP_TOKEN;
+    const [messageText, setMessageText] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [messages, setMessages] = useState([]);
+    const endOfMessagesRef = useRef(null);
+
+    //Side effect for getting messages from database
+    useEffect(() => {
+        const messageRef = collection(
+            db,
+            "messages",
+            "ashish.blackhawk@gmail.com",
+            `91${phoneNumber}`
+        );
+        const getMessagesQuery = query(messageRef, orderBy("timestamp"));
+        const unsubscribe = onSnapshot(
+            getMessagesQuery,
+            (querySnapshot) => {
+                setMessages(querySnapshot.docs.map((doc) => doc.data()));
+                setTimeout(() => {
+                    scrollToBottom();
+                }, 500);
+            },
+            (queryError) => {
+                console.log("Query Error", queryError);
+            }
+        );
+
+        return () => {
+            unsubscribe();
+        };
+    }, [phoneNumber]);
+
+    //function for scrolling to bottom of screen
+    const scrollToBottom = () => {
+        endOfMessagesRef.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    };
+
+    //function for sending messages
+    const sendMessage = async () => {
+        try {
+            console.log(bearer);
+            console.log(`trying...`);
+            console.log(phoneNumber);
+            console.log(messageText);
+            const response = await axios.post(
+                "https://graph.facebook.com/v16.0/111369568531545/messages",
+                {
+                    messaging_product: "whatsapp",
+                    recipient_type: "individual",
+                    to: `91${phoneNumber}`,
+                    type: "text",
+                    text: {
+                        // the text object
+                        preview_url: false,
+                        body: `${messageText}`,
+                    },
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${bearer}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log(response.data);
+
+            const outgoingDataToFirestore = {
+                messageId: response.data.messages[0].id,
+                category: "OUTGOING",
+                type: "text",
+                status: "sending",
+                statusUpdatedOn: `${Timestamp.now().seconds}`,
+                timestamp: `${Timestamp.now().seconds}`,
+                body: `${messageText}`,
+            };
+            const messageRef = doc(
+                db,
+                "messages",
+                "ashish.blackhawk@gmail.com",
+                `91${phoneNumber}`,
+                response.data.messages[0].id
+            );
+            await setDoc(messageRef, outgoingDataToFirestore, {
+                merge: true,
+            });
+            setMessageText("");
+            scrollToBottom();
+        } catch (error) {
+            console.error(error);
         }
-      );
-      console.log(response.data);
-      setMessageText("");
-      setPhoneNumber("");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter Phone Number..."
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Type your message here..."
-        value={messageText}
-        onChange={(e) => setMessageText(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send</button>
-    </div>
-  );
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Enter Phone Number..."
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Type your message here..."
+                value={messageText}
+                onChange={(e) => setMessageText(e.target.value)}
+            />
+            <button onClick={sendMessage}>Send</button>
+            {phoneNumber && messages.length && (
+                <>
+                    {console.log(messages)}
+                    {messages?.map((message) => (
+                        <p>{message.body}</p>
+                    ))}
+                    <div ref={endOfMessagesRef}>----------</div>
+                </>
+            )}
+        </div>
+    );
 }
